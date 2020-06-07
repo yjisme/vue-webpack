815e2a83a564238457d160f38402eeef5dd7e0d4
yjisme <277739025@qq.com>
2020-06-07 15:35:43 +0800

    first-version

---
+++ webpack.prod.js
@@ -0,0 +1,21 @@
+const merge = require("webpack-merge");
+const baseConfig = require("./webpack.config.js");
+
+const prodConfig = {
+  mode: "production",
+  devtool: "none",
+  optimization: {
+    splitChunks: {
+      //分包配置
+      chunks: "all",
+      cacheGroups: {
+        styles: {
+          minSize: 0,
+          test: /\.css$/,
+          minChunks: 2,
+        },
+      },
+    },
+  },
+};
+module.exports = merge(baseConfig, prodConfig);
